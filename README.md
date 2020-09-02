f194d4d049f244242a0f1ba9c29fc0917e471544
nghiepnq-dgh <nghiepnq@digitechglobalco.com>
2020-09-02 20:41:48 +0700

    [Feature] Add module message

--- README.md
+++ README.md
@@ -70,6 +70,11 @@ Nest is an MIT-licensed open source project. It can grow thanks to the sponsors 
 - Website - [https://nestjs.com](https://nestjs.com/)
 - Twitter - [@nestframework](https://twitter.com/nestframework)
 
+
+## Generate Module
+- cmd: nest generate module|controller|service :name
+
+
 ## License
 
   Nest is [MIT licensed](LICENSE).
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { MessageModule } from './message/message.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { MongooseModule } from '@nestjs/mongoose';
       isGlobal: true,
     }),
     MongooseModule.forRoot('mongodb://127.0.0.1:27017/chatbox'),
+    MessageModule,
   ],
   controllers: [AppController],
   providers: [AppService],

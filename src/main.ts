42b95620945d636b21be83de1ce5a013690a198f
nghiepnq-dgh <nghiepnq@digitechglobalco.com>
2020-09-03 15:32:58 +0700

    [Improve] Add Swagger Document

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,37 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './exceptions/http-exception.filter';
+import { Logger } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
   app.enableCors();
+  app.setGlobalPrefix('v1');
+  initialSwagger(app);
   await app.listen(process.env.PORT);
+  Logger.log(`
+  =============================================
+
+  Server is running on: ${await app.getUrl()}
+
+  =============================================
+  `);
+}
+
+function initialSwagger(app: NestExpressApplication): void {
+  const options = new DocumentBuilder()
+    .setTitle('API Document')
+    .setDescription('The document about list of API for Chat Box')
+    .setVersion('v1.0')
+    // .setContact('Chat Box')
+    .addBasicAuth()
+    .addBearerAuth()
+    .setBasePath('v1')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-doc', app, document);
 }
 bootstrap();
